dc8ddc7873a4b8431cd3ab16c955508c111a715a
Keagan Leon <74585425+keaganleon745@users.noreply.github.com>
2025-06-12 15:41:01 -0700

    Create 1_osint_recon.sh

---
+++ 1_osint_recon.sh
@@ -0,0 +1,11 @@
+# WHOIS lookup
+whois bankofamerica.com
+
+# DNS lookup
+dig bankofamerica.com any
+
+# Passive subdomain enumeration
+sublist3r -d bankofsimulatedcorp.local
+
+# Harvest email and hostnames using Google/Bing
+theHarvester -d bankofsimulatedcorp.local -b google
